eb24db282ac2b2eaf69566a7d5dff18f316195a7
Yustinus Radityo P <radityopradana@yahoo.co.id>
2018-03-31 02:23:43 +0700

    Default commit

--- bot.js
+++ bot.js
@@ -1,52 +1,52 @@
-// 'use strict';
+'use strict';
 
-// const line = require('@line/bot-sdk');
-// const express = require('express');
+const line = require('@line/bot-sdk');
+const express = require('express');
 
-// // create LINE SDK config from env variables
-// const config = {
-//   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-//   channelSecret: process.env.CHANNEL_SECRET,
-// };
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
 
-// // create LINE SDK client
-// const client = new line.Client(config);
+// create LINE SDK client
+const client = new line.Client(config);
 
-// // create Express app
-// // about Express itself: https://expressjs.com/
-// const app = express();
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
 
-// // register a webhook handler with middleware
-// // about the middleware, please refer to doc
-// app.post('/callback', line.middleware(config), (req, res) => {
-//   Promise
-//     .all(req.body.events.map(handleEvent))
-//     .then((result) => res.json(result))
-//     .catch((err) => {
-//       console.error(err);
-//       res.status(500).end();
-//     });
-// });
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
 
-// // event handler
-// function handleEvent(event) {
-//   if (event.type !== 'message' || event.message.type !== 'text') {
-//     // ignore non-text-message event
-//     return Promise.resolve(null);
-//   }
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
 
-//   // create a echoing text message
-//   const echo = { type: 'text', text: event.message.text };
+  // create a echoing text message
+  const echo = { type: 'text', text: 'salahnya dimana?' };
 
-//   // use reply API
-//   return client.replyMessage(event.replyToken, echo);
-// }
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
 
-// // listen on port
-// const port = process.env.PORT || 3000;
-// app.listen(port, () => {
-//   console.log(`listening on ${port}`);
-// });
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
 // //   //Akan mendengarkan message yang dimulai dengan tanda '!'
 // //   if(message.author.equals(bot.user)) return;
 
@@ -110,69 +110,69 @@
 // //   // }
 // // });
 // // bot.login(TOKEN);
-'use strict';
+// 'use strict';
 
-const line = require('@line/bot-sdk');
-const express = require('express');
-// const https = require('https');
-// const url = "https://radityo.000webhostapp.com/index.php?nama=";
-
-
-//Inisialiasi Bot Line
-const config = {
-	channelAccessToken : process.env.BOT_TOKEN;
-	channelSecret : process.env.BOT_SECRET;
-}
-const client = new line.Client(config);
-
-const app = express();
-
-app.post('/callback',line.middleware(config),(req,res)=>{
-	Promise
-	.all(req.body.events.map(handleEvent))
-	.then((result)=>res.json(result))
-	.catch((err)=>{
-		console.error(err);
-		res.status(500).end();
-	});
-
-});
-
-function handleEvent(evt){
-	if(event.type!=='message'|| event.message.type!=='text'){
-		return Promise.resolve(null);
-	}
-	
-	// var baru = new Date();
-	// var tanggal = baru.getDate();
-	// var bulan = baru.getMonth();
-	// if(event.type=='message'){
-		if(event.message.text.substring(0,4)==dosen){
-			const namaDosen = event.message.substring(5);
-			const dosen = {type:'text',text: namaDosen};
-		}
-	 else{
-		const dosen= {type:'text',text: 'error'};
-	}
-	return client.replyMessage(event.replyToken,dosen);
-}
+// const line = require('@line/bot-sdk');
+// const express = require('express');
+// // const https = require('https');
+// // const url = "https://radityo.000webhostapp.com/index.php?nama=";
 
 
-const port= process.env.PORT || 3000;
-app.listen(port,()=>{
-	console.log(`listening on ${port}`);
-});
-// // var req = http.request(options,function(res){
-// // 	var msg = '';
+// //Inisialiasi Bot Line
+// const config = {
+// 	channelAccessToken : process.env.BOT_TOKEN;
+// 	channelSecret : process.env.BOT_SECRET;
+// }
+// const client = new line.Client(config);
 
-// // 	res.setEncoding('utf8');
-// // 	res.on('data',)
-// // });
-// bot.on('ready',function(){
-// 	console.log("Ready");
-// // 	logger.info('Connected');
-// // 	logger.info('Logged in as: ');
-// // 	logger.info(bot.username + ' - (' + bot.id + ')');	
+// const app = express();
+
+// app.post('/callback',line.middleware(config),(req,res)=>{
+// 	Promise
+// 	.all(req.body.events.map(handleEvent))
+// 	.then((result)=>res.json(result))
+// 	.catch((err)=>{
+// 		console.error(err);
+// 		res.status(500).end();
+// 	});
+
 // });
-// bot.on('message',function(message){
-// 	//Bot harus tau kapan mengeksekusi command
+
+// function handleEvent(evt){
+// 	if(event.type!=='message'|| event.message.type!=='text'){
+// 		return Promise.resolve(null);
+// 	}
+	
+// 	// var baru = new Date();
+// 	// var tanggal = baru.getDate();
+// 	// var bulan = baru.getMonth();
+// 	// if(event.type=='message'){
+// 		if(event.message.text.substring(0,4)==dosen){
+// 			const namaDosen = event.message.substring(5);
+// 			const dosen = {type:'text',text: namaDosen};
+// 		}
+// 	 else{
+// 		const dosen= {type:'text',text: 'error'};
+// 	}
+// 	return client.replyMessage(event.replyToken,dosen);
+// }
+
+
+// const port= process.env.PORT || 3000;
+// app.listen(port,()=>{
+// 	console.log(`listening on ${port}`);
+// });
+// // // var req = http.request(options,function(res){
+// // // 	var msg = '';
+
+// // // 	res.setEncoding('utf8');
+// // // 	res.on('data',)
+// // // });
+// // bot.on('ready',function(){
+// // 	console.log("Ready");
+// // // 	logger.info('Connected');
+// // // 	logger.info('Logged in as: ');
+// // // 	logger.info(bot.username + ' - (' + bot.id + ')');	
+// // });
+// // bot.on('message',function(message){
+// // 	//Bot harus tau kapan mengeksekusi command
