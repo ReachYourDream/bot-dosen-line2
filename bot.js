0d7ae1647c5b73780e2cd5ef5f2ec1aededa36c8
Yustinus Radityo P <radityopradana@yahoo.co.id>
2018-04-05 21:35:15 +0700

    Default commit

--- bot.js
+++ bot.js
@@ -62,8 +62,8 @@ function handleEvent(event) {
     }
     const namaDosen = b.substring(6);
     const urlDosen = url+namaDosen;
-    echo = { type:'text', text: 'Mohon menunggu' };
-    client.replyMessage(event.replyToken,echo);
+    // echo = { type:'text', text: 'Mohon menunggu' };
+    // client.replyMessage(event.replyToken,echo);
     status = 0;
     var detik= 0;
     // return client.replyMessage(event.replyToken, echo);
