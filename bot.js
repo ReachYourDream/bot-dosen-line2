537d231877a9625384e23350def24b9cb286941e
Yustinus Radityo P <radityopradana@yahoo.co.id>
2018-04-06 21:18:42 +0700

    Default commit

--- bot.js
+++ bot.js
@@ -90,7 +90,7 @@ function handleEvent(event) {
                 for(x = 0; x<body['jumlah'];x++){
                   str = str+ x-1 + '. ' + body['nama'][x] + body['status'][x] + '\n';
                 }
-                echo.text = 'Terdapat ' + body['jumlah'] + ' dosen dengan nama \"' + $namaDosen + '\" Yaitu:\n' + str;
+                echo.text = 'Terdapat ' + body['jumlah'] + ' dosen dengan nama \"' + namaDosen + '\" Yaitu:\n' + str;
               } else{
               echo.text= 'Nama Dosen: ' + body['nama'] + '  Status: ' + body['status'];
               }
